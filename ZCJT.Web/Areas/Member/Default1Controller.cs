f387ce8fc9cc9947cf251835d13bd560207aa848
ZengLi <378139086@qq.com>
2016-02-04 14:09:34 +0800

    ZCJT

---
+++ ZCJT.Web/Areas/Member/Default1Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZCJT.Web.Areas.Member
+{
+    public class Default1Controller : Controller
+    {
+        //
+        // GET: /Member/Default1/
+
+        public ActionResult Index()
+        {
+            @{
+    ViewBag.Title = "会员中心";
+}
+
+<h2>欢迎你！@User.Identity.Name 
+    </h2>
+            return View();
+        }
+
+    }
+}
